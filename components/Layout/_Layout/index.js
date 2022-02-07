0f0c7366677b0dc2b70c6a3b3224ca4a6925d945
Jen Hail <hello@jenniferhail.com>
2022-02-06 21:17:31 -0500

    adding favicon

--- components/Layout/_Layout/index.js
+++ components/Layout/_Layout/index.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/favicon.ico" />
         {/* <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
